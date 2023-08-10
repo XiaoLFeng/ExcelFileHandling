ad57426befcc34317cb35b523afb96b6f56c20a7
XiaoLFeng <gm@x-lf.cn>
2023-08-10 17:01:17 +0800

    Initial commit

---
+++ src/main/java/com/wxjw/ExcelFileHandlingApplication.java
@@ -0,0 +1,18 @@
+package com.wxjw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringBoot启动
+ *
+ * @author 筱锋xiao_lfeng
+ */
+@SpringBootApplication
+public class ExcelFileHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelFileHandlingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wxjw/ExcelFileHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxjw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelFileHandlingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
